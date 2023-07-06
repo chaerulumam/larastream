e0de39ab108ed31377708b2946b389743fac0d4f
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-06 21:35:35 +0700

    create migration table for database

--- database/migrations/2023_07_06_142738_create_packages_table.php
+++ database/migrations/2023_07_06_142738_create_packages_table.php
@@ -11,14 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('packages', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->float('price');
+            $table->integer('max_days');
+            $table->integer('max_users');
+            
+            $table->boolean('is_download');
+            $table->boolean('is_4k');
+
             $table->timestamps();
         });
     }
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('packages');
     }
 };
--- database/migrations/2023_07_06_143259_create_user_premiums_table.php
+++ database/migrations/2023_07_06_143259_create_user_premiums_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('user_premiums', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+
+            $table->foreignId('package_id')->constrained('packages');
+            $table->foreignId('user_id')->constrained('users');
+
+            $table->date('end_of_subscription');
+
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('user_premiums');
     }
 };
