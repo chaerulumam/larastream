7a0434dd6ade34e6d7f2ee946dcc98e1dc35b9bd
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-08 16:05:44 +0700

    create validation for stpre movie CRUD

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMovieRequest;
 
 class MovieController extends Controller
 {
@@ -15,5 +17,37 @@ class MovieController extends Controller
     public function create()
     {
         return view('admin.movies.movie-create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->except('_token');
+
+        $request->validate([
+            'title' => 'required|string',
+            'small_thumbnail' => 'required|image|mimes:jpeg,jpg,png',
+            'large_thumbnail' => 'required|image|mimes:jpeg,jpg,png',
+            'trailer' => 'required|url',
+            'movie' => 'required|url',
+            'casts' => 'required|string',
+            'categories' => 'required|string',
+            'release_date' => 'required|string',
+            'about' => 'required|string',
+            'short_about' => 'required|string',
+            'duration' => 'required|string',
+            'featured' => 'required|string',
+        ]);
+
+        $smallThumbnail = $request->small_thumbnail;
+        $largeThumbnail = $request->large_thumbnail;
+
+        $originalSmallThumbnailName = Str::random(10). '-' .$smallThumbnail->getClientOriginalName();
+        $originalLargeThumbnailName = Str::random(10). '-' .$largeThumbnail->getClientOriginalName();
+
+        $smallThumbnail->storeAs('public/thumbnail', $originalSmallThumbnailName);
+        $largeThumbnail->storeAs('public/thumbnail', $originalLargeThumbnailName);
+
+        // dd($originalLargeThumbnailName);
+        dd($originalSmallThumbnailName);
     }
 }
