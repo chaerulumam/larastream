6e2b64d7b7d7e3950e673f6e1f9c5edede032060
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-10 20:06:04 +0700

    integration login page

--- app/Http/Controllers/Member/LoginController.php
+++ app/Http/Controllers/Member/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Member;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
@@ -12,5 +13,28 @@ class LoginController extends Controller
      public function index()
     {
         return view('member.login');
+    }
+
+    public function auth(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        $credentials['role'] = 'member';
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return "success";
+
+            return redirect()->route('/');
+        }
+
+        return back()->withErrors([
+            'credentials' => 'The credentials are not match'
+        ])->withInput();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Admin\TransactionController;
-use App\Http\Controllers\Member\LoginController as LoginMemberLoginController;
+use App\Http\Controllers\Member\LoginController as MemberLoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,5 +44,6 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('register', [RegisterController::class, 'index'])->name('member.register');
 Route::post('register', [RegisterController::class, 'store'])->name('member.register.store');
 
-Route::get('login', [LoginMemberLoginController::class, 'index'])->name('member.login');
+Route::get('login', [MemberLoginController::class, 'index'])->name('member.login');
+Route::post('login', [MemberLoginController::class, 'auth'])->name('member.login.auth');
 
