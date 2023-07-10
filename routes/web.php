c22fb7d402196e3a65fe0660b235598c67c3826d
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-10 21:20:56 +0700

    integration dashboard movie and display data

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Member\DashboardController;
 use App\Http\Controllers\Member\LoginController as MemberLoginController;
 
 /*
@@ -46,4 +47,8 @@ Route::post('register', [RegisterController::class, 'store'])->name('member.regi
 
 Route::get('login', [MemberLoginController::class, 'index'])->name('member.login');
 Route::post('login', [MemberLoginController::class, 'auth'])->name('member.login.auth');
+
+Route::group(['prefix' => 'member'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('member.dashboard');
+});
 
