3d9c523be453166b852a272d3ceaa65f01ef0fed
Chaerul Umam <chaerulumamb244@gmail.com>
2023-07-13 19:54:57 +0700

    integration page subscription

---
+++ app/Http/Controllers/Member/SubscriptionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserPremium;
+use Illuminate\Http\Request;
+
+class SubscriptionController extends Controller
+{
+    public function index()
+    {
+        $userId = auth()->user()->id;
+
+        $user_premiums = UserPremium::with('package')->where('user_id', $userId)->first();
+
+        // dd($userId);
+
+        if (!$user_premiums) {
+            return redirect()->route('pricing');
+        }
+
+        return view('member.subscription', compact('user_premiums'));
+    }
+}
